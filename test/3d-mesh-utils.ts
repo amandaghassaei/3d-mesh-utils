994bb3831f176e7bb79fa3884245f9a4ad73df84
amandaghassaei <amandaghassaei@gmail.com>
2023-07-05 23:41:47 -0700

    tests

--- test/3d-mesh-utils.ts
+++ test/3d-mesh-utils.ts
@@ -1,16 +1,19 @@
 import { expect } from 'chai';
 import { readFileSync } from 'fs';
 import { parseSTL } from '@amandaghassaei/stl-parser';
+import { parseMsh } from 'msh-parser';
 import {
 	calcBoundingBox,
 	calcEdgesFromIndexedFaces,
 	calcEdgesFromNonIndexedFaces,
 	mergeVertices,
 	scaleVerticesToUnitBoundingBox,
+	calcEdgesFromNestedIndexedFaces,
 } from '../src/3d-mesh-utils';
 
 const cubeAscii = readFileSync('./test/stl/cubeAscii.stl');
 const cubeBinary = readFileSync('./test/stl/cubeBinary.stl');
+const bunnyMsh = readFileSync('./test/msh/stanford_bunny.msh');
 
 describe('3d-mesh-utils', () => {
 	it('mergeVertices() - merges vertices and indexed faces', async () => {
@@ -25,7 +28,7 @@ describe('3d-mesh-utils', () => {
 			2, 7, 5, 1, 0, 6, 6, 0, 4
 		]));			
 	});
-	it('calcEdgesFromNonIndexedFaces(), calcEdgesFromIndexedFaces() - calculates edges', () => {
+	it('calcEdgesFromNonIndexedFaces(), calcEdgesFromIndexedFaces(), calcEdgesFromNestedIndexedFaces() - calculates edges', () => {
 		{
 			const mesh = parseSTL(cubeAscii);
 			const edgesNonIndexed = calcEdgesFromNonIndexedFaces(mesh);
@@ -69,6 +72,14 @@ describe('3d-mesh-utils', () => {
 				6, 6, 3, 7, 3, 7, 2, 0, 6
 			]);
 			expect(edgesIndexed.length).to.equal(mesh.faceIndices.length);
+		}
+		{
+			const mesh = parseMsh(bunnyMsh);
+			const edges = calcEdgesFromNestedIndexedFaces({ faceIndices: mesh.exteriorFaces });
+			expect(edges.slice(0, 100)).to.deep.equal([
+				2, 6, 6, 3, 3, 2, 0, 3, 3, 9, 9, 0, 9, 14, 14, 0, 1, 4, 4, 2, 2, 1, 1, 13, 13, 4, 0, 1, 2, 0, 4, 5, 5, 2, 5, 6, 4, 8, 8, 5, 6, 12, 12, 3, 12, 9, 4, 19, 19, 8, 5, 11, 11, 6, 11, 12, 12, 17, 17, 9, 9, 18, 18, 14, 17, 18, 1, 15, 15, 16, 16, 1, 12, 6341, 6341, 17, 7, 22, 22, 13, 13, 7, 18, 23, 23, 14, 15, 21, 21, 45, 45, 15, 18, 98, 98, 23, 12, 53, 53, 6341
+			]);
+			expect(edges.length).to.equal(42072);
 		}
 	});
 	it('calcBoundingBox() - calculates bounding box', () => {
